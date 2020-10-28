7680b713cd1a0e38a243e7f97434aec0e60d0339
Christopher Liu <christopherliu@sorcero.com>
2020-10-28 15:17:10 -0400

    Improve tester

--- Tester.java
+++ Tester.java
@@ -1,8 +1,23 @@
 public class Tester {
     public static void main(String[] args) {
+        int err = 0;
         Point p1 = new Point(1, 2);
         Point p2 = new Point(p1);
-        System.out.println(p2.getX());
-        System.out.println(p1.getY());
+
+        err += check("p1.getX()", p1.getX(), 1.0);
+        err += check("p2.getY()", p2.getY(), 2.0);
+
+        if (err == 0) System.out.println("All good!");
+        else if (err == 1) System.out.println("Uh oh... 1 error found.");
+        else System.out.println("Uh oh... " + err + " errors found.");
+    }
+
+    public static int check(String name, Object actual, Object expected) {
+        if (actual.equals(expected)) return 0;
+        else {
+            System.out.println("Failure on " + name + ": Expected \"" +
+                                expected + "\", got \"" + actual + "\".");
+            return 1;
+        }
     }
 }
