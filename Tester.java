82f7552a8f213ecae566d639633674ee5940f039
Christopher Liu <christopherliu@sorcero.com>
2020-10-28 15:20:45 -0400

    Add distanceTo method

--- Tester.java
+++ Tester.java
@@ -3,9 +3,11 @@ public class Tester {
         int err = 0;
         Point p1 = new Point(1, 2);
         Point p2 = new Point(p1);
+        Point p3 = new Point(5, 4);
 
         err += check("p1.getX()", p1.getX(), 1.0);
         err += check("p2.getY()", p2.getY(), 2.0);
+        err += check("p1.distanceTo(p3)", p1.distanceTo(p3), 4.47213595499958);
 
         if (err == 0) System.out.println("All good!");
         else if (err == 1) System.out.println("Uh oh... 1 error found.");
