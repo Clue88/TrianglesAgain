7ddfe87e85b742ba22734bb66c5537896ffe3329
Christopher Liu <christopherliu@sorcero.com>
2020-10-28 17:01:26 -0400

    Add area method

--- Tester.java
+++ Tester.java
@@ -3,15 +3,17 @@ public class Tester {
         int err = 0;
         Point p1 = new Point(1, 2);
         Point p2 = new Point(p1);
-        Point p3 = new Point(5, 4);
-        Point p4 = new Point(10, 5);
+        Point p3 = new Point(8, 3);
+        Point p4 = new Point(15, -3);
         Triangle t1 = new Triangle(p1, p3, p4);
 
         err += check("p1.getX()", p1.getX(), 1.0);
         err += check("p2.getY()", p2.getY(), 2.0);
-        err += check("p1.distanceTo(p3)", p1.distanceTo(p3), 4.47213595499958);
+        err += check("p1.distanceTo(p3)", p1.distanceTo(p3), 7.0710678118654755);
         err += check("p1.equals(p2)", p1.equals(p2), true);
         err += check("p1.equals(p3)", p1.equals(p3), false);
+        err += check("t1.getPerimeter()", t1.getPerimeter(), 31.156681016476867);
+        err += check("t1.getPerimeter()", t1.getArea(), 24.499999999999975);
 
         if (err == 0) System.out.println("All good!");
         else if (err == 1) System.out.println("Uh oh... 1 error found.");
